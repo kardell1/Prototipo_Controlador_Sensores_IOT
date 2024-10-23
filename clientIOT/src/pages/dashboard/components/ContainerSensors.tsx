445bc4d154c7be8ccb7cc0ecbdb9f1d74e5b8cde
kardell1 <kevindavidsilva97@gmail.com>
2024-10-23 16:11:49 -0300

    ProyectoEricka

--- clientIOT/src/pages/dashboard/components/ContainerSensors.tsx
+++ clientIOT/src/pages/dashboard/components/ContainerSensors.tsx
@@ -3,9 +3,9 @@ import { SensorCard } from "./SensorCard";
 export const ContainerSensors = () => {
   return (
     <div className="flex flex-row max-sm:flex-col w-full justify-evenly max-md:items-center max-md:justify-center p-4 gap-5 ">
-      <SensorCard name="Humedad" topic="esp32/ResHumedad" />
-      <SensorCard name="Suelo" topic="esp32/ResHumedadSuelo" />
-      <SensorCard name="Temperatura" topic="esp32/ResTemperatura" />
+      <SensorCard name="Humedad" topic="esp32/ResHumedad" type="%" />
+      <SensorCard name="Suelo" topic="esp32/ResHumedadSuelo" type="%" />
+      <SensorCard name="Temperatura" topic="esp32/ResTemperatura" type="°C" />
     </div>
   );
 };
--- clientIOT/src/pages/dashboard/components/SensorCard.tsx
+++ clientIOT/src/pages/dashboard/components/SensorCard.tsx
@@ -3,19 +3,34 @@ import { getSocket } from "../../../context/ConexionSocket";
 type PropsCardSensor = {
   topic: string;
   name: string;
+  type: string;
 };
-export const SensorCard = ({ name, topic }: PropsCardSensor) => {
+export const SensorCard = ({ name, topic, type }: PropsCardSensor) => {
   const socket = getSocket();
   const [list, setList] = useState<string[]>([]);
+  const [status, setStatus] = useState(false);
   useEffect(() => {
     const handleSensorData = (value: string) => {
-      setList((oldList) => {
-        const newList = [...oldList, value];
-        if (newList.length > 10) {
-          return newList.slice(1);
-        }
-        return newList;
-      });
+      if (isNaN(parseInt(value))) {
+        setStatus(false);
+        setList((oldList) => {
+          const newList = [...oldList, "SensorErratico "];
+          if (newList.length > 10) {
+            return newList.slice(1);
+          }
+          return newList;
+        });
+      } else {
+        setStatus(true);
+        setList((oldList) => {
+          const newList = [...oldList, value + type];
+          if (newList.length > 10) {
+            return newList.slice(1);
+          }
+          return newList;
+        });
+      }
+      
     };
     socket?.on(topic, handleSensorData);
     return () => {
@@ -25,7 +40,10 @@ export const SensorCard = ({ name, topic }: PropsCardSensor) => {
 
   return (
     <section className="p-5 w-[25%] mt-5 rounded-lg bg-emerald-400 min-w-max max-md:w-[70%] ">
-      <p className="text-xl font-medium text-center">{name} </p>
+      <div className="flex justify-around items-center  " >
+        <p className="text-xl font-medium text-center">{name} </p>
+        <span className={`w-6 h-6 rounded-full shadow-xl  ${status? "bg-green-600" : "bg-red-600" } `} ></span>
+      </div>
       <div>
         <ul className="text-center  ">
           {list.length > 10 ? (
@@ -40,7 +58,11 @@ export const SensorCard = ({ name, topic }: PropsCardSensor) => {
                     </li>
                   );
                 } else {
-                  return <li key={index}>{value}</li>;
+                  return (
+                    <li key={index}>
+                      {value} 
+                    </li>
+                  );
                 }
               })}
             </>
